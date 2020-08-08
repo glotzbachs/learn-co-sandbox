ccac703fa4c16220b5b2a2bfbb7f68190c51bd51
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-08-08 20:41:07 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,20 @@
+ruby learning-comments.rb
+case greeting
+  when "unfriendly"
+    puts "What do you want?"
+  when "friendly"
+    puts "Hey! How are ya?"
+end
+
+case name
+  when "Alice"
+    puts "Hey Alice!"
+  when "Rabbit"
+    puts "Don't be late!"
+  when "Mad Hatter"
+    puts "Join us for tea?"
+  when "Queen of Hearts"
+    puts "CHOP OFF HEAD!!!"
+  else
+    puts "Whooo are youuu?"
+  end
